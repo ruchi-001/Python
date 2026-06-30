bfc03f1009958d7fe523b3644b9ecab1361d7c67
ruchi-001 <ruchikumari2214@gmail.com>
2026-06-30 22:16:31 +0530

    Add number guessing game

---
+++ GuessNumber.py
@@ -0,0 +1,31 @@
+import random
+
+print("Hi! Welcome to the Number Guessing Game. Let's start")
+
+low = int(input("Enter the Lower Bound: "))
+high = int(input("Enter the Upper Bound: "))
+
+print(f"\nYou have 7 chances to guess the number between {low} and {high}. Let's start!")
+
+num = random.randint(low, high) 
+# Total allowed chances
+chances = 5
+# Guess counter
+guessCount = 0                         
+
+while guessCount < chances:
+    guessCount += 1
+    guess = int(input('Enter your guess: '))
+
+    if guess == num:
+        print(f'Correct! The number is {num}. You guessed it in {guessCount} attempts.')
+        break
+
+    elif guessCount >= chances and guess != num:
+        print(f'Sorry! The number was {num}. Better luck next time.')
+
+    elif guess > num:
+        print('Too high! Try a lower number.')
+
+    elif guess < num:
+        print('Too low! Try a higher number.')
